032480973b03ae687fa6a7ddb8ec1c291a689c76
Belen Linera <belen.linera@gmail.com>
2022-08-10 16:50:29 -0300

    es div x 7 duda

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -9,10 +9,7 @@ Restricción: Usar el operador de multiplicación
 lado_cuadrado = 5
 
 #
-<<<<<<< HEAD
- area_cuadrado = lado_cuadrado * lado_cuadrado1
-=======
-area_cuadrado= lado_cuadrado * lado_cuadrado
+area_cuadrado = lado_cuadrado * lado_cuadrado
 # COMPLETAR - FIN
 assert area_cuadrado == 25
 
@@ -24,7 +21,7 @@ Re-Escribir usando el operador de potencia.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado ** 2
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -37,7 +34,7 @@ Re-Escribir usando la función pow.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = pow(lado_cuadrado, 2)
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -52,7 +49,7 @@ precio = 3.74
 presupuesto_disponible = 10
 
 # COMPLETAR - INICIO
-
+cantidad_a_comprar = presupuesto_disponible // precio
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -66,7 +63,7 @@ Restricción: Usar el operador módulo.
 numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
-
+es_divisible_por_siete = (numero_incalculable % 7)
 # COMPLETAR - FIN
 
 assert es_divisible_por_siete
